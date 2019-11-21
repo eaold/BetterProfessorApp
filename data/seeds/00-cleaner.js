82d058f98d114e6a24700d2b08c563ca23ae38f0
EmilyAbrahart <emilyabrahart@gmail.com>
2019-11-21 15:40:33 +0000

    Add cleaner seed for seeding on heroku

---
+++ data/seeds/00-cleaner.js
@@ -0,0 +1,17 @@
+const cleaner = require('knex-cleaner');
+
+exports.seed = function(knex) {
+	return knex('projects')
+		.truncate()
+		.then(() => {
+			return knex('students')
+				.truncate()
+				.then(() => {
+					return cleaner.clean(knex, {
+						mode: 'truncate',
+						restartIdentity: true,
+						ignoreTables: ['knex_migrations', 'knex_migrations_lock']
+					});
+				});
+		});
+};
